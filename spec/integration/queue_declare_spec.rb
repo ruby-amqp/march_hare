b4712a455488661abb6def948243813f66fd2cb9
Michael S. Klishin <michael@novemberain.com>
2011-09-22 23:18:49 +0400

    Use after :each so that RSpec execution thread is not blocked forever

--- spec/integration/queue_declare_spec.rb
+++ spec/integration/queue_declare_spec.rb
@@ -4,12 +4,27 @@ require "spec_helper"
 describe "Queue" do
   context "with a server-generated name" do
     let(:connection) { HotBunnies.connect }    
-    let(:channel) { connection.create_channel }
+    let(:channel)    { connection.create_channel }
 
-    it "can be declared" do
-      channel.queue("", :auto_delete => true)
+    after :each do
       channel.close
       connection.close
+    end
+
+    it "can be declared as auto-deleted" do
+      channel.queue("", :auto_delete => true)
+    end
+
+    it "can be declared as auto-deleted and non-durable" do
+      channel.queue("", :auto_delete => true, :durable => false)
+    end
+
+    it "can be declared as NON-auto-deleted" do
+      channel.queue("", :auto_delete => false)
+    end
+
+    it "can be declared as NON-durable" do
+      channel.queue("", :durable => false)
     end
   end
 end
