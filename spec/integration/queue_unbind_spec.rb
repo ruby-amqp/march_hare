b20f58b1150696fc901c4f38b5e1c5e4ce46433f
Michael S. Klishin <michael@novemberain.com>
2011-09-23 18:00:24 +0400

    Basic test coverage for queue.unbind

---
+++ spec/integration/queue_unbind_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+
+
+describe "Any queue" do
+
+  #
+  # Environment
+  #
+
+  let(:connection) { HotBunnies.connect }
+  let(:channel)    { connection.create_channel }
+
+  after :each do
+    channel.close
+    connection.close
+  end
+
+
+  #
+  # Examples
+  #
+
+  it "can be unbound from amq.fanout" do
+    exchange = channel.exchange("amq.fanout", :type => :fanout, :durable => true, :auto_delete => false)
+    queue    = channel.queue("", :auto_delete => true)
+
+    queue.bind(exchange)
+
+    exchange.publish("", :routing_key => queue.name)
+    queue.get.should_not be_nil
+
+    queue.unbind(exchange)
+
+    exchange.publish("")
+    queue.get.should be_nil
+  end
+
+
+  it "can be unbound from a client-declared exchange" do
+    exchange = channel.exchange("hot.bunnies.fanout", :type => :fanout, :durable => false, :auto_delete => true)
+    queue    = channel.queue("", :auto_delete => true)
+
+    queue.bind(exchange)
+
+    exchange.publish("", :routing_key => queue.name)
+    queue.get.should_not be_nil
+
+    queue.unbind(exchange)
+
+    exchange.publish("")
+    queue.get.should be_nil
+  end
+end
