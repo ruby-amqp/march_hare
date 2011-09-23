7c30a4233b83fadd9e811640df32c504b5aa05c3
Michael S. Klishin <michael@novemberain.com>
2011-09-23 18:33:06 +0400

    Basic test coverage for queue.purge

---
+++ spec/integration/queue_purge_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+
+describe "Any queue" do
+
+  #
+  # Environment
+  #
+
+  let(:connection) { HotBunnies.connect }
+  let(:channel)    { connection.create_channel }
+
+  after :each do
+    channel.close
+    connection.close
+  end
+
+
+  #
+  # Examples
+  #
+
+  it "can be purged" do
+    exchange = channel.exchange("amq.fanout", :type => :fanout, :durable => true, :auto_delete => false)
+    queue    = channel.queue("", :auto_delete => true)
+    exchange.publish("")
+    queue.get.should be_nil
+    queue.purge
+    queue.get.should be_nil
+
+    queue.bind(exchange)
+
+    exchange.publish("", :routing_key => queue.name)
+    queue.get.should_not be_nil
+    queue.purge
+    queue.get.should be_nil
+  end
+end
