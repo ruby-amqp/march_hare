79d023cecd539ab6426268561d2c8913b5ed6ffd
Michael S. Klishin <michael@novemberain.com>
2011-12-05 01:35:14 +0400

    Demonstrate round robin message distribution between multiple consumers that share a queue

--- spec/integration/basic_consume_spec.rb
+++ spec/integration/basic_consume_spec.rb
@@ -1,5 +1,59 @@
 require "spec_helper"
 
+describe "Multiple non-exclusive consumers per queue" do
+  let(:connection) { HotBunnies.connect }
+  let(:channel)    { connection.create_channel }
+
+  after :each do
+    channel.close
+    connection.close
+  end
+
+  context "on the same channel (so prefetch levels won't affect message distribution)" do
+    it "have messages distributed to them in the round robin manner" do
+      n                = 100
+      mailbox1         = []
+      mailbox2         = []
+      mailbox3         = []
+
+      all_received     = java.util.concurrent.CountDownLatch.new(50)
+      consumer_channel = connection.create_channel
+
+      queue            = channel.queue("", :auto_delete => true)
+
+      consumer1        = queue.subscribe(:blocking => false) do |metadata, payload|
+        mailbox1 << payload
+        all_received.count_down
+      end
+      consumer2        = queue.subscribe(:blocking => false) do |metadata, payload|
+        mailbox2 << payload
+        all_received.count_down
+      end
+      consumer3        = queue.subscribe(:blocking => false) do |metadata, payload|
+        mailbox3 << payload
+        all_received.count_down
+      end
+
+
+      sleep 1.5 # let consumers in other threads start.
+      n.times do |i|
+        channel.default_exchange.publish("Message #{i}", :routing_key => queue.name)
+      end
+
+      all_received.await
+
+      mailbox1.size.should >= 33
+      mailbox2.size.should >= 33
+      mailbox3.size.should >= 33
+
+      consumer1.shutdown!
+      consumer2.shutdown!
+      consumer3.shutdown!
+    end
+  end
+end
+
+
 describe "Queue consumer" do
   let(:connection) { HotBunnies.connect }
   let(:channel)    { connection.create_channel }
