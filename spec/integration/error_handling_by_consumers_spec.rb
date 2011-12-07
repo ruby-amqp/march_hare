267f67494a786a986b025ee87721d07fbe897e25
Michael S. Klishin <michael@novemberain.com>
2011-12-08 00:06:19 +0400

    Commit this stub file [ci skip]

---
+++ spec/integration/error_handling_by_consumers_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "An AMQP consumer" do
+  let(:connection) { HotBunnies.connect }
+  let(:channel)    { connection.create_channel }
+
+  after :each do
+    channel.close
+    connection.close
+  end
+
+  
+end
