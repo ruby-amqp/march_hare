ca632f5c5e4498ec7559962444adfe5ac9396092
Michael Klishin <michael@defprotocol.org>
2013-05-24 11:45:07 +0400

    Turn HotBunnies::Channel into a Ruby class; introduce HotBunnies::Session
    This is the first step towards hiding RabbitMQ Java client parts that
    are unlikely to be useful or even understandable to many
    Ruby developers:
    
     * methods overloaded on both arity and type signatures
     * Java exceptions raised on protocol errors
     * com.rabbitmq.client.Channel interface
    
    Because Ruby won't allow us overload methods on arity and/or type signatures,
    lower-level methods such as HB::Channel#queue_declare only have a single most
    commonly used signature and uses the same default argument values as
    overloaded versions in the Java channel implementation.
    
    It passes existing HotBunnies test suite without modifications.
    
    Note that this also drops support for the :internal option for exchanges
    that's not supposed to be used by app developers.

--- lib/hot_bunnies.rb
+++ lib/hot_bunnies.rb
@@ -4,99 +4,11 @@ require 'java'
 require 'ext/commons-io'
 require 'ext/rabbitmq-client'
 
+require 'hot_bunnies/session'
 
 module HotBunnies
-  java_import 'com.rabbitmq.client.ConnectionFactory'
-  java_import 'com.rabbitmq.client.Connection'
-  java_import 'com.rabbitmq.client.Channel'
-  java_import 'com.rabbitmq.client.DefaultConsumer'
-  java_import 'com.rabbitmq.client.AMQP'
-
-  def self.connect(options={})
-    cf = ConnectionFactory.new
-
-    cf.uri                = options[:uri]          if options[:uri]
-    cf.host               = hostname_from(options) if include_host?(options)
-    cf.port               = options[:port]         if options[:port]
-    cf.virtual_host       = vhost_from(options)    if include_vhost?(options)
-    cf.connection_timeout = timeout_from(options)  if include_timeout?(options)
-    cf.username           = username_from(options) if include_username?(options)
-    cf.password           = password_from(options) if include_password?(options)
-
-    cf.requested_heartbeat = heartbeat_from(options)          if include_heartbeat?(options)
-    cf.connection_timeout  = connection_timeout_from(options) if include_connection_timeout?(options)
-
-    tls = (options[:ssl] || options[:tls])
-    case tls
-    when true then
-      cf.use_ssl_protocol
-    when String then
-      if options[:trust_manager]
-        cf.use_ssl_protocol(tls, options[:trust_manager])
-      else
-        cf.use_ssl_protocol(tls)
-      end
-    end
-
-    cf.new_connection
-  end
-
-  protected
-
-  def self.hostname_from(options)
-    options[:host] || options[:hostname] || ConnectionFactory.DEFAULT_HOST
-  end
-
-  def self.include_host?(options)
-    !!(options[:host] || options[:hostname])
-  end
-
-  def self.vhost_from(options)
-    options[:virtual_host] || options[:vhost] || ConnectionFactory.DEFAULT_VHOST
-  end
-
-  def self.include_vhost?(options)
-    !!(options[:virtual_host] || options[:vhost])
-  end
-
-  def self.timeout_from(options)
-    options[:connection_timeout] || options[:timeout]
-  end
-
-  def self.include_timeout?(options)
-    !!(options[:connection_timeout] || options[:timeout])
-  end
-
-  def self.username_from(options)
-    options[:username] || options[:user] || ConnectionFactory.DEFAULT_USER
-  end
-
-  def self.heartbeat_from(options)
-    options[:heartbeat_interval] || options[:requested_heartbeat] || ConnectionFactory.DEFAULT_HEARTBEAT
-  end
-
-  def self.connection_timeout_from(options)
-    options[:connection_timeout_interval] || options[:connection_timeout] || ConnectionFactory.DEFAULT_CONNECTION_TIMEOUT
-  end
-
-  def self.include_username?(options)
-    !!(options[:username] || options[:user])
-  end
-
-  def self.password_from(options)
-    options[:password] || options[:pass] || ConnectionFactory.DEFAULT_PASS
-  end
-
-  def self.include_password?(options)
-    !!(options[:password] || options[:pass])
-  end
-
-  def self.include_heartbeat?(options)
-    !!(options[:heartbeat_interval] || options[:requested_heartbeat] || options[:heartbeat])
-  end
-
-  def self.include_connection_timeout?(options)
-    !!(options[:connection_timeout_interval] || options[:connection_timeout])
+  def self.connect(*args)
+    Session.connect(*args)
   end
 end
 
--- lib/hot_bunnies/channel.rb
+++ lib/hot_bunnies/channel.rb
@@ -1,10 +1,31 @@
 # encoding: utf-8
 
 module HotBunnies
-  module Channel
-    def queue(name, options={})
-      Queue.new(self, name, options)
+  class Channel
+
+    # Wraps a com.rabbitmq.client.ChannelN instance
+    def self.wrap(java_channel)
+      new(java_channel)
     end
+
+    def initialize(delegate)
+      @delegate = delegate
+    end
+
+    def id
+      @delegate.channel_number
+    end
+
+    def channel_number
+      @delegate.channel_number
+    end
+
+    def close(code = 200, reason = "Goodbye")
+      @delegate.close(code, reason)
+    end
+
+
+    # @group Exchanges
 
     def exchange(name, options={})
       exchange = Exchange.new(self, name, options)
@@ -16,6 +37,64 @@ module HotBunnies
       self.exchange("", :durable => true, :auto_delete => false, :type => "direct")
     end
 
+    def exchange_declare(name, type, durable = false, auto_delete = false, arguments = nil)
+      @delegate.exchange_declare(name, type, durable, auto_delete, arguments)
+    end
+
+    # @endgroup
+
+
+    # @group Queues
+
+    def queue(name, options={})
+      Queue.new(self, name, options)
+    end
+
+    def queue_declare(name, durable, exclusive, auto_delete, arguments = {})
+      @delegate.queue_declare(name, durable, exclusive, auto_delete, arguments)
+    end
+
+    def queue_declare_passive(name)
+      @delegate.queue_declare_passive(name)
+    end
+
+    def queue_delete(name, if_empty = false, if_unused = false)
+      @delegate.queue_delete(name, if_empty, if_unused)
+    end
+
+    def queue_bind(queue, exchange, routing_key, arguments = nil)
+      @delegate.queue_bind(queue, exchange, routing_key, arguments)
+    end
+
+    def queue_unbind(queue, exchange, routing_key, arguments = nil)
+      @delegate.queue_unbind(queue, exchange, routing_key, arguments)
+    end
+
+    def queue_purge(name)
+      @delegate.queue_purge(name)
+    end
+
+    # @endgroup
+
+
+    # @group basic.*
+
+    def basic_publish(exchange, routing_key, mandatory, immediate, properties, body)
+      @delegate.basic_publish(exchange, routing_key, mandatory, immediate, properties, body)
+    end
+
+    def basic_get(queue, auto_ack)
+      @delegate.basic_get(queue, auto_ack)
+    end
+
+    def basic_consume(queue, auto_ack, consumer)
+      @delegate.basic_consume(queue, auto_ack, consumer)
+    end
+
+    def basic_qos(prefetch_count)
+      @delegate.basic_qos(prefetch_count)
+    end
+
     def qos(options={})
       if options.size == 1 && options[:prefetch_count]
       then basic_qos(options[:prefetch_count])
@@ -24,11 +103,7 @@ module HotBunnies
     end
 
     def prefetch=(n)
-      qos(:prefetch_count => n)
-    end
-
-    def on_return(&block)
-      self.set_return_listener(block)
+      basic_qos(n)
     end
 
     def ack(delivery_tag, multiple = false)
@@ -43,5 +118,54 @@ module HotBunnies
     def nack(delivery_tag, multiple = false, requeue = false)
       basic_nack(delivery_tag, multiple, requeue)
     end
+
+    def basic_recover(requeue = true)
+      @delegate.basic_recover(requeue)
+    end
+
+    def basic_recover_async(requeue = true)
+      @delegate.basic_recover_async(requeue)
+    end
+
+    # @endgroup
+
+
+    def confirm_select
+      @delegate.confirm_select
+    end
+
+    def wait_for_confirms
+      @delegate.wait_for_confirms
+    end
+
+    def next_publisher_seq_no
+      @delegate.next_publisher_seq_no
+    end
+
+    def tx_select
+      @delegate.tx_select
+    end
+
+    def tx_commit
+      @delegate.tx_commit
+    end
+
+    def tx_rollback
+      @delegate.tx_rollback
+    end
+
+    def channel_flow(active)
+      @delegate.channel_flow(active)
+    end
+
+
+    def on_return(&block)
+      self.set_return_listener(block)
+    end
+
+    def method_missing(selector, *args)
+      @delegate.__send__(selector, *args)
+    end
+
   end
 end
