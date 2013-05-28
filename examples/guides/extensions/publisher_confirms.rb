965e51a8a37a7dc40e494936083acc5b38c44acf
Michael Klishin <michael@defprotocol.org>
2013-05-28 13:19:06 +0400

    Port more extension examples

---
+++ examples/guides/extensions/publisher_confirms.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "rubygems"
+require "hot_bunnies"
+
+puts "=> Demonstrating publisher confirms"
+puts
+
+conn = HotBunnies.connect
+
+ch   = conn.create_channel
+x    = ch.fanout("amq.fanout")
+q    = ch.queue("", :exclusive => true).bind(x)
+
+ch.confirm_select
+1000.times do
+  x.publish("")
+end
+ch.wait_for_confirms # blocks calling thread until all acks are received
+
+sleep 0.2
+puts "Received acks for all published messages. #{q.name} now has #{q.message_count} messages."
+
+sleep 0.7
+puts "Disconnecting..."
+conn.close
