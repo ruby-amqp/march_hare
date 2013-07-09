a0f8f6213689fbc949db231a1bc4c7d25b153a3c
Michael Klishin <michael@defprotocol.org>
2013-07-09 20:50:44 +0400

    Ditto

--- lib/hot_bunnies.rb
+++ lib/hot_bunnies.rb
@@ -8,8 +8,14 @@ require 'hot_bunnies/version'
 require 'hot_bunnies/exceptions'
 require 'hot_bunnies/session'
 
+# HotBunnies is a JRuby client for RabbitMQ built on top of the official Java client.
+#
+# @see HotBunnies.connect
+# @see HotBunnies::Session
+# @see HotBunnies::Channel
 module HotBunnies
   # Delegates to {HotBunnies::Session.connect}
+  # @see HotBunnies::Session.connect
   def self.connect(*args)
     Session.connect(*args)
   end
