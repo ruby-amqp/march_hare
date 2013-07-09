d4ef21fa8e111895211f8976d8010b72394cc586
Michael Klishin <michael@defprotocol.org>
2013-07-09 22:05:08 +0400

    Ditto

--- lib/hot_bunnies/channel.rb
+++ lib/hot_bunnies/channel.rb
@@ -247,6 +247,20 @@ module HotBunnies
       end
     end
 
+    # Declares a topic exchange or looks it up in the cache of previously
+    # declared exchanges.
+    #
+    # @param [String] name Exchange name
+    # @param [Hash] opts Exchange parameters
+    #
+    # @option opts [Boolean] :durable (false) Should the exchange be durable?
+    # @option opts [Boolean] :auto_delete (false) Should the exchange be automatically deleted when no longer in use?
+    # @option opts [Hash] :arguments ({}) Optional exchange arguments (used by RabbitMQ extensions)
+    #
+    # @return [HotBunnies::Exchange] Exchange instance
+    # @see http://hotbunnies.info/articles/exchanges.html Exchanges and Publishing guide
+    # @see http://hotbunnies.info/articles/extensions.html RabbitMQ Extensions to AMQP 0.9.1 guide
+    # @api public
     def topic(name, opts = {})
       Exchange.new(self, name, opts.merge(:type => "topic")).tap do |x|
         x.declare!
