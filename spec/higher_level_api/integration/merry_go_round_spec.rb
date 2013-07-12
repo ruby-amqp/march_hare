11bbc07852a322aaab2b034b691341e4fa0d8eb4
Michael Klishin <michael@defprotocol.org>
2013-07-12 06:10:34 +0400

    Experiment with more stages in this test

--- spec/higher_level_api/integration/merry_go_round_spec.rb
+++ spec/higher_level_api/integration/merry_go_round_spec.rb
@@ -27,7 +27,7 @@ describe "A message that is proxied by multiple intermediate consumers" do
     end
   end
 
-  # message flow is as follows:
+  # the message flow is as follows:
   #
   # x => q4 => q3 => q2 => q1 => xs (results)
   it "reaches its final destination" do
@@ -65,9 +65,9 @@ describe "A message that is proxied by multiple intermediate consumers" do
       x.publish("msg #{i}", :routing_key => q4.name)
     end
 
-    t = n / 1000 * 2.0
-    puts "About to sleep for #{t} seconds..."
-    sleep(t)
+    until xs.size == n
+      sleep 0.5
+    end
 
     xs.size.should == n
     xs.last.should == "msg #{n - 1}"
@@ -77,6 +77,57 @@ describe "A message that is proxied by multiple intermediate consumers" do
     end
 
     [ch1, ch2, ch3, ch4, ch5].each do |ch|
+      ch.close
+    end
+  end
+
+
+
+  it "can stretch tens of stages" do
+    s   = 32
+    n   = 1000
+    xs  = []
+    chs = []
+    qs  = []
+    cs  = []
+
+    s.times do |i|
+      ch = c1.create_channel
+      chs << ch
+
+      next_q = qs.last
+
+      q  = ch.queue("", :exclusive => true)
+      qs << q
+
+      cs << q.subscribe do |_, payload|
+        if next_q
+          next_q.publish(payload)
+        else
+          xs << payload
+        end
+      end
+    end
+
+    pch = c2.create_channel
+    x   = pch.default_exchange
+
+    n.times do |i|
+      x.publish("msg #{i}", :routing_key => qs.last.name)
+    end
+
+    until xs.size == n
+      sleep 0.5
+    end
+
+    xs.size.should == n
+    xs.last.should == "msg #{n - 1}"
+
+    cs.each do |cons|
+      cons.cancel
+    end
+
+    chs.each do |ch|
       ch.close
     end
   end
