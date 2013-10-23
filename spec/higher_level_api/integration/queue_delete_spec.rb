68f53164fadcfdbfb1c195f72662639b956ce58a
Michael Klishin <mklishin@gopivotal.com>
2013-10-23 21:02:35 +0400

    Adapt queue.delete tests to RabbitMQ 3.2 behavior

--- spec/higher_level_api/integration/queue_delete_spec.rb
+++ spec/higher_level_api/integration/queue_delete_spec.rb
@@ -24,14 +24,13 @@ describe "Queue" do
   end
 
   context "that DOES NOT exist" do
-    it "cannot be deleted" do
+    it "raises NO exception (as of RabbitMQ 3.2)" do
       ch = connection.create_channel
       q  = ch.queue("")
 
       q.delete(true, true)
-      lambda do
-        q.delete(true, true)
-      end.should raise_error(MarchHare::NotFound)
+      # No exception as of RabbitMQ 3.2. MK.
+      q.delete(true, true)
     end
   end
 end
