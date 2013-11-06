b8b029da5036c6ad24500f13c104adb46d8796e9
Noah <noah-junk@magram.org>
2013-11-06 15:55:23 -0800

    add support for ConfirmListener

--- spec/higher_level_api/integration/publisher_confirmations_spec.rb
+++ spec/higher_level_api/integration/publisher_confirmations_spec.rb
@@ -28,4 +28,19 @@ describe "Any channel" do
 
     true.should be_true
   end
+  
+  it "can receive publisher confirmation acks" do
+    got_ack = false
+    ch = connection.create_channel
+    q  = ch.queue("", :exclusive => true)
+
+    ch.confirm_select
+    ch.on_confirm { |type, seq, multiple| got_ack = (type ==:ack) }
+    
+    ch.default_exchange.publish("", :routing_key => q.name)
+
+    ch.wait_for_confirms(40)
+
+    got_ack.should be_true
+  end
 end
