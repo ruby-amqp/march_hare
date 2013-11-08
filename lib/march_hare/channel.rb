c64684b3ca6873c3eb14002a291d26a79d99155c
Michael Klishin <michael@clojurewerkz.org>
2013-11-08 16:23:55 +0400

    whitespace

--- lib/march_hare/channel.rb
+++ lib/march_hare/channel.rb
@@ -804,7 +804,7 @@ module MarchHare
     def on_return(&block)
       self.add_return_listener(BlockReturnListener.from(block))
     end
-    
+
     # Defines a publisher confirm handler
     def on_confirm(&block)
       self.add_confirm_listener(BlockConfirmListener.from(block))
@@ -813,16 +813,16 @@ module MarchHare
     def method_missing(selector, *args)
       @delegate.__send__(selector, *args)
     end
-    
+
 
     #
     # Implementation
     #
-    
+
     # @private
     class BlockConfirmListener
       include com.rabbitmq.client.ConfirmListener
-      
+
       def self.from(block)
         new(block)
       end
